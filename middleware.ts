a086607325682c1ee04d20fcc0b51bae01e1d005
branditweb <branditweb@users.noreply.github.com>
2026-03-06 15:28:42 +0100

    Limit auth middleware to /admin paths

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,11 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (!pathname.startsWith("/admin")) {
+    return NextResponse.next();
+  }
+
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!token) {
